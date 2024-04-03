66f69cb87d3d8d41926f395f257182eafd7a7504
Markus Tobias Geschonneck <markus_tobias.geschonneck@smail.th-koeln.de>
2024-04-03 14:35:46 +0200

    Erweiterung der Optionen für die Genauigkeitsberechnung

--- accuracy_calculations/accuracy_calculations/accuracy_calculation_options.py
+++ accuracy_calculations/accuracy_calculations/accuracy_calculation_options.py
@@ -1,5 +1,18 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Union, Literal
+
+
+@dataclass
+class AccuracySubcategoryWeights:
+    missing_data: float = 1
+    outliers_intrinsic: float = 1
+    dispersion_intrinsic: float = 1
+
+    outliers_comparison: float = 2
+    dispersion_comparison: float = 2
+
+    def sum_weights(self) -> float:
+        return sum(self.__dict__.values())
 
 
 @dataclass
@@ -7,4 +20,9 @@ class AccuracyCalculationOptions:
     mode_for_dispersion_identification: Union[Literal['mean'],
                                               Literal['regression_deg_2'],
                                               Literal['regression_deg_3']] = 'mean'
+    threshold_outliers: float = 2.58
+
     plot_outliers: bool = False
+
+    weights: AccuracySubcategoryWeights = field(
+        default_factory=AccuracySubcategoryWeights)
