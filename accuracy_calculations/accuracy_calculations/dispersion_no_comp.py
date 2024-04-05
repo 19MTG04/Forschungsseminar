5426f29f9fdc0f7916c5980bd9eb39f3847b1436
Markus Tobias Geschonneck <markus_tobias.geschonneck@smail.th-koeln.de>
2024-04-05 19:32:08 +0200

    Überarbeitung der Methode zur Berechnung der internen Streuung.

--- accuracy_calculations/accuracy_calculations/dispersion_no_comp.py
+++ accuracy_calculations/accuracy_calculations/dispersion_no_comp.py
@@ -1,10 +1,10 @@
-from typing import Tuple
+from typing import Tuple, Any
 import numpy as np
 import pandas as pd
 
 
 from accuracy_calculations.accuracy_calculation_options import AccuracyCalculationOptions
-from accuracy_calculations.statistical_analysis_helper import optimize_window_length, analyse_dispersion
+from accuracy_calculations.statistical_analysis_helper import optimize_window_length, analyse_dispersion, return_without_outliers
 
 
 def analyse_dispersion_intrinsic(data_series: pd.Series, options: AccuracyCalculationOptions) -> Tuple[float, pd.Series]:
@@ -13,19 +13,53 @@ def analyse_dispersion_intrinsic(data_series: pd.Series, options: AccuracyCalcul
         data_series, window_length=window_length, options=options)
 
     dispersion_score = get_dispersion_stats(
-        z_score, options)
+        data_series, approximation_curve, z_score, options, window_length)
 
     return dispersion_score, approximation_curve
 
 
-def get_dispersion_stats(z_score: pd.Series, options: AccuracyCalculationOptions) -> float:
-    z_score_normalized = abs(z_score) / options.threshold_outliers
-    z_score_clipped = z_score_normalized.clip(0, 1)
+def get_dispersion_stats(data_series: pd.Series, approximation: pd.Series,
+                         z_score: pd.Series, options: AccuracyCalculationOptions,
+                         window_length: int) -> float:
+    data_without_outlieres = return_without_outliers(
+        data_series, approximation, z_score, options)
 
-    dispersion_score = sum(np.cos(np.pi / 2 * z_score_clipped)
-                           ) / len(z_score_clipped.dropna())
+    data_rolling_window = data_without_outlieres.rolling(
+        window=window_length, min_periods=1, center=True)
+    approximation_rolling_window = approximation.rolling(
+        window=window_length, min_periods=1, center=True)
+
+    difference_start_end_approx_windows = abs(approximation_rolling_window.apply(
+        calculate_difference_window_start_end))
+    absolute_difference_sum_data = data_rolling_window.apply(
+        calculate_sum_absolute_difference_between_each_window_entry)
+
+    # Dieses Verhältnis wird klein (nahe 0) für verrauschte und groß (nahe 1) für weniger verrauschte Zeitreihen.
+    # Das liegt daran, dass die Differenz von Start und Ende der Fenster der Approximation relativ resistent gegen das Rauschen ist
+    # und ein Maßstab dafür, wie viel Veränderung der Größenordnungen in den Daten rauschfrei zu erwarten ist.
+    # Im Gegensatz dazu ist bei Betrachtung der Unterschiede zwischen jedem Wert in jedem Fenster der Realdaten
+    # ein besonders großer Wert bei viel Rauschen zu erwarten.
+    ratio_dispersion = difference_start_end_approx_windows.sum() / \
+        absolute_difference_sum_data.sum()
+
+    # Empirische Gleichung zur Bewertung des vorher eingeführten Verhältnisses.
+    dispersion_score = (-np.exp(-4 * ratio_dispersion) + 1) / (-np.exp(-4) + 1)
 
     return dispersion_score
+
+
+def calculate_difference_window_start_end(window: Any):
+    difference = window.iloc[-1] - window.iloc[0]
+    if np.isnan(difference):
+        raise ValueError("Unhandeld NaN")
+    return difference
+
+
+def calculate_sum_absolute_difference_between_each_window_entry(window: Any):
+    sum_abs_diff = window.diff().abs().sum()
+    if np.isnan(sum_abs_diff):
+        raise ValueError("Unhandeld NaN")
+    return sum_abs_diff
 
 
 if __name__ == '__main__':
