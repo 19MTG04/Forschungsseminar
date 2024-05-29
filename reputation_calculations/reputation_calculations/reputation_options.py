19ed865a9556a756e0a1d7a8b53b35ddc59bfb56
Markus Tobias Geschonneck <markus_tobias.geschonneck@smail.th-koeln.de>
2024-05-29 11:37:36 +0200

    Verbesserung, sodass nicht beide Argumente übergebbar sind. Außerdem Docstrings und Numpy Funktion statt Liste.

--- reputation_calculations/reputation_calculations/reputation_options.py
+++ reputation_calculations/reputation_calculations/reputation_options.py
@@ -10,10 +10,26 @@ class ReputationOptions:
     exponential_smoothing_factor: float = 0.1
 
 
-def create_reputation_options(ratings_array: Optional[np.ndarray] = None, length_series: int = 100) -> ReputationOptions:
+def create_reputation_options(ratings_array: Optional[np.ndarray] = None, length_series: Optional[int] = None) -> ReputationOptions:
+    """
+    Erzeugt ein ReputationOptions-Objekt entweder basierend auf einem gegebenen Ratings-Array
+    oder durch Generierung einer zufälligen Serie.
+
+    Args:
+        ratings_array (Optional[np.ndarray]): Ein Array von Bewertungen, falls vorhanden.
+        length_series (Optional[int]): Die Länge der zu generierenden Serie, falls kein ratings_array gegeben ist.
+
+    Returns:
+        ReputationOptions: Ein Objekt mit dem Ratings-Array und Standardwerten.
+    """
+    if ratings_array is not None and length_series is not None:
+        raise ValueError(
+            "Es macht keinen Sinn sowohl ratings_array als auch length_series zu belegen. Entweder wird ein Array verarbeitet oder eine Zufallsserie vorgegebener Länge erstellt.")
+
     if ratings_array is not None:
         pass
     else:
+        length_series = length_series if length_series is not None else 100
         ratings_array = create_random_series(length_series=length_series)
 
     options = ReputationOptions(ratings_array)
@@ -25,10 +41,9 @@ def create_random_series(length_series: int) -> np.ndarray:
     np.random.seed(1)
 
     # Trend nachbilden und mit Zufallszahlen abändern
-    trend = [0 + (i**2) * 0.5 / length_series**2 for i in range(length_series)]
-    ratings_array = np.add(np.random.uniform(
-        low=-0.2, high=0.2, size=length_series), trend)
-
-    ratings_array = np.clip(ratings_array, 0, 1)
+    trend = np.array([0 + (i**2) * 0.5 / length_series **
+                     2 for i in range(length_series)])
+    random_values = np.random.uniform(low=-0.2, high=0.2, size=length_series)
+    ratings_array = np.clip(trend + random_values, 0, 1)
 
     return ratings_array
