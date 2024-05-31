d097bcd9ba51c2448c8f05e076fc2a139f7304b6
Markus Tobias Geschonneck <markus_tobias.geschonneck@smail.th-koeln.de>
2024-05-31 14:33:32 +0200

    Hinzufügen der Optionen bei Datenqualitätsberechnung

---
+++ data_quality_calculations/data_quality_calculations/data_quality_options.py
@@ -0,0 +1,40 @@
+from enum import Enum, auto
+from dataclasses import dataclass
+
+from accuracy_calculations.accuracy_calculation_options import AccuracyCalculationOptions
+
+from believability_calculations.believability_options import BelievabilityOptions
+
+from objectivity_calculations.objectivity_options import ObjectivityCalculationOptions
+
+from reputation_calculations.reputation_options import ReputationOptions
+
+
+@dataclass
+class CategoryWeightsDataQuality:
+    weight_accuracy: float = 1
+    weight_believability: float = 1
+    weight_objectivity: float = 1
+    weight_reputation: float = 1
+
+
+@dataclass
+class DataQualityOptions:
+    accuracy_options: AccuracyCalculationOptions
+    believability_options: BelievabilityOptions
+    objectivity_options: ObjectivityCalculationOptions
+    reputation_options: ReputationOptions
+
+
+class AutoName(Enum):
+
+    # Overrides the auto() function to automatically set the value of the enum
+    # variable to the string name.
+    @staticmethod
+    def _generate_next_value_(name: str, start, count, last_values):
+        return name.lower()
+
+
+class ModelType(str, AutoName):
+    FULL_MODEL = auto()
+    MODEL_WO_COMMUNITY = auto()
