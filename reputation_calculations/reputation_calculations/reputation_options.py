0243f8cecf141e8e32cafdf0beb13995168a912b
Markus Tobias Geschonneck <markus_tobias.geschonneck@smail.th-koeln.de>
2024-05-31 11:03:07 +0200

    Kleinigkeit in Funktionsaufruf.

--- reputation_calculations/reputation_calculations/reputation_options.py
+++ reputation_calculations/reputation_calculations/reputation_options.py
@@ -20,7 +20,7 @@ def create_reputation_options(ratings_array: Optional[np.ndarray] = None, length
         length_series (Optional[int]): Die Länge der zu generierenden Serie, falls kein ratings_array gegeben ist.
 
     Returns:
-        ReputationOptions: Ein Objekt mit dem Ratings-Array und Standardwerten.
+        ReputationOptions: Ein Objekt mit dem Ratings-Array und den anderen Klassenwerten.
     """
     if ratings_array is not None and length_series is not None:
         raise ValueError(
@@ -30,15 +30,16 @@ def create_reputation_options(ratings_array: Optional[np.ndarray] = None, length
         pass
     else:
         length_series = length_series if length_series is not None else 100
-        ratings_array = create_random_series(length_series=length_series)
+        ratings_array = create_random_series(
+            length_series=length_series, seed=1)
 
     options = ReputationOptions(ratings_array)
     return options
 
 
-def create_random_series(length_series: int) -> np.ndarray:
+def create_random_series(length_series: int, seed: int = 1) -> np.ndarray:
     # Reproduzierbare Ergebnisse mit Zufallszahlen immer über seed steuern
-    np.random.seed(1)
+    np.random.seed(seed)
 
     # Trend nachbilden und mit Zufallszahlen abändern
     trend = np.array([0 + (i**2) * 0.5 / length_series **
