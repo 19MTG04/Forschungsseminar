b7230e392db2a24d513634add368a5ff5223710f
Markus Tobias Geschonneck <markus_tobias.geschonneck@smail.th-koeln.de>
2024-06-27 16:40:05 +0200

    Verebsserter Aufruf der Main.

--- data_quality_calculations/data_quality_calculations/main_full_data_quality_calculation.py
+++ data_quality_calculations/data_quality_calculations/main_full_data_quality_calculation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from typing import Tuple, Union
 
 from data_quality_calculations.data_quality_options import CategoryWeightsDataQuality, ModelType, DataQualityOptions
@@ -57,9 +58,22 @@ def calculate_final_score(model_type: ModelType, data_series: pd.Series, compari
     return data_quality_score, (accuracy_score, believability_score, objectivity_score, reputation_score)
 
 
+def print_results(channel_group, data_quality_score, category_scores):
+    if isinstance(category_scores[3], str):
+        reputation_score = category_scores[3]
+    else:
+        reputation_score = f'{category_scores[3]:.3f}'
+
+    print(f'Datenqualitäts-Score ZR {channel_group}: {data_quality_score:.3f}\n'
+          f'Genauigkeit: {category_scores[0]:.3f},\n'
+          f'Glaubwürdigkeit: {category_scores[1]:.3f},\n'
+          f'Objektivität: {category_scores[2]:.3f},\n'
+          f'Ruf: {reputation_score}')
+
+
 if __name__ == '__main__':
 
-    channel_group = 280
+    channel_group = 261
     observation_feature = "Antrieb 1  Drehzahl"
 
     model_type = ModelType.MODEL_WO_COMMUNITY
@@ -70,6 +84,11 @@ if __name__ == '__main__':
     #     period_limitations_same_dataset=[
     #         ('12.10.2023 14:45:00', '15.10.2023 16:38:00')],
     #     period_limitations_additional_dataset=[('20.09.2023 08:50:00', '23.09.2023 18:52:00'), ('29.09.2023 16:00:00', '06.10.2023 06:59:00'), ('06.10.2023 22:02:00', '07.10.2023 02:48:00')])
+    # Bsp: Cwälz, Ohne Minimum Länge
+    comparison_data_options = ComparisonDataExtractionOptions(
+        period_limitations_same_dataset=[
+            ('12.10.2023 14:59:00', '15.10.2023 16:38:00')],
+        period_limitations_additional_dataset=[('20.09.2023 08:50:00', '23.09.2023 18:52:00'), ('29.09.2023 16:00:00', '06.10.2023 06:59:00'), ('06.10.2023 22:02:00', '07.10.2023 02:48:00')])
 
     # # Bsp Cbetr, Minimum Länge 100sec
     # comparison_data_options = ComparisonDataExtractionOptions(
@@ -78,10 +97,10 @@ if __name__ == '__main__':
     #         ('16.10.2023 06:18:00', '17.10.2023 07:21:00')],
     #     period_limitations_additional_dataset=[('25.09.2023 16:01:00', '28.09.2023 16:22:00'), ('28.09.2023 16:23:00', '29.09.2023 15:59:00'), ('05.10.2023 14:40:00', '06.10.2023 22:01:00'), ('07.10.2023 02:49:00', '09.10.2023 22:39:00')])
     # Bsp Cbetr, Ohne Minimum Länge
-    comparison_data_options = ComparisonDataExtractionOptions(
-        period_limitations_same_dataset=[
-            ('16.10.2023 06:18:00', '17.10.2023 07:21:00')],
-        period_limitations_additional_dataset=[('25.09.2023 16:01:00', '28.09.2023 16:22:00'), ('28.09.2023 16:23:00', '29.09.2023 15:59:00'), ('05.10.2023 14:40:00', '06.10.2023 22:01:00'), ('07.10.2023 02:49:00', '09.10.2023 22:39:00')])
+    # comparison_data_options = ComparisonDataExtractionOptions(
+    #     period_limitations_same_dataset=[
+    #         ('16.10.2023 06:18:00', '17.10.2023 07:21:00')],
+    #     period_limitations_additional_dataset=[('25.09.2023 16:01:00', '28.09.2023 16:22:00'), ('28.09.2023 16:23:00', '29.09.2023 15:59:00'), ('05.10.2023 14:40:00', '06.10.2023 22:01:00'), ('07.10.2023 02:49:00', '09.10.2023 22:39:00')])
 
     data_series, comparison_data = extract_data_and_comparison_data(
         channel_group, observation_feature, comparison_data_options)
@@ -98,13 +117,4 @@ if __name__ == '__main__':
     data_quality_score, category_scores = calculate_final_score(
         model_type, data_series, comparison_data, data_quality_weights, comparison_data_options, data_quality_options)
 
-    if isinstance(category_scores[3], str):
-        reputation_score = category_scores[3]
-    else:
-        reputation_score = f'{category_scores[3]:.3f}'
-
-    print(f'Datenqualitäts-Score ZR {channel_group}: {data_quality_score:.3f}\n'
-          f'Genauigkeit: {category_scores[0]:.3f},\n'
-          f'Glaubwürdigkeit: {category_scores[1]:.3f},\n'
-          f'Objektivität: {category_scores[2]:.3f},\n'
-          f'Ruf: {reputation_score}')
+    print_results(channel_group, data_quality_score, category_scores)
